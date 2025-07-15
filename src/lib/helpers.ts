17c5023943818a34fd791fd604a7516003d9852e
ATEMMIX <91466481+atemmix@users.noreply.github.com>
2025-07-15 13:10:02 +0300

    Delete src directory

--- src/lib/helpers.ts
+++
@@ -1,6 +0,0 @@
-const formatter = new Intl.NumberFormat("en-US", {
-  notation: "compact",
-  compactDisplay: "short",
-});
-
-export const compactNumber = (number: number) => formatter.format(number);
